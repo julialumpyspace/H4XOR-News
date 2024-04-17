83b22497f72cc797c64563941b54bcf2af9f8975
yuliiavorobei <juligumenyk@gmail.com>
2024-04-17 15:08:32 +0300

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Юлія Воробей on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
